ac66c79c5dbcdcdeb571a294acc857efcc072b67
Anthony <fassetar@gmail.com>
2019-04-12 23:22:12 -0400

    started notes.

---
+++ index.js
@@ -0,0 +1,13 @@
+
+//dollars per day in a low cost city.
+travel_low = 45;
+travel_high = 55;
+full_low = 75;
+full_high = 85;
+
+//NOTES: 
+// 1st & n^Last = "travel"
+// n = single date counted once.
+// p is the next project.
+// (n^(n+1) == p^first) || (n == p) = "full"
+//  n^last > n < 1st = "full"
