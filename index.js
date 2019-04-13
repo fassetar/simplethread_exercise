814cb667fd6f27d9b59fe90d743c0436b008a0f9
Anthony <fassetar@gmail.com>
2019-04-13 10:49:01 -0400

    going to seperate this more but quick & dirty.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 *@autor: Anthony Fassett
 *@description: A calcuator for figuring out a clients reimbursement.
@@ -5,10 +6,10 @@
 */
 
 //dollars per day in a low cost city.
-travel_low = 45;
-travel_high = 55;
-full_low = 75;
-full_high = 85;
+var travel_low = 45;
+var travel_high = 55;
+var full_low = 75;
+var full_high = 85;
 
 //NOTES: 
 // 1st & n^Last = "travel"
@@ -17,29 +18,43 @@ full_high = 85;
 // m is the number of days between projects
 // (n^(n+1) == p^first) || (n == p) = "full"
 // this is when m is greater than 1.
-// m > 1 n^last = "travel" & p^first = "travel"
+// m > 1; n^last = "travel" & p^first = "travel"
 //  n^last > n < 1st = "full" == m;
 
-const express = require('express');
-const app = express();
-const fs = require("fs");
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var http = require('http');
 
 
-app.get('/api/dates', (req, res) => {
-
-   fs.readFile( __dirname +'/data' +".json", 'utf8', function (err, data) {
-       res.send(data);
-       res.end( data );
-   });
-
+app.get('/api/dates', function (req, res) {
+  fs.readFile(__dirname + '/data' + ".json", 'utf8', function (err, data) {
+    res.send(data);
+    res.end(data);
+  });
 });
 
 
-var server = app.listen(8081, () => {
-  var port = server.address().port
-  console.log("Example app listening at http://127.0.0.1:%s", port)
+var server = app.listen(8081, function () {
+  var port = server.address().port;
+  console.log("app listening at http://127.0.0.1:%s", port);
+
+  var request = require('request');
+  request({ 
+    url: 'http://127.0.0.1:8081/api/dates',
+    json: true
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body); // Print the google web page.
+    }
+  });
 });
 
+var reimburseCalcuator = function (data) {
+  data.forEach(element => {
+    console.log(element);
+  });
+};
 
 
 //Questions 
@@ -47,5 +62,4 @@ var server = app.listen(8081, () => {
 //    overlap which one rate is taken? Probably the higher one.
 // - "If there is a gap between projects, then the days on
 //    either side of that gap are travel days. Does that mean
-//    in addition one day is included for the travel days or 
-//    is this just first to last of a project?
+//    in addition to one day or is this just first to last of a project?
