f0a453051ee7de17544693823b654f4f8a7ce600
Anthony <fassetar@gmail.com>
2019-04-12 23:31:04 -0400

    final notes, sleepy time.

--- index.js
+++ index.js
@@ -9,5 +9,20 @@ full_high = 85;
 // 1st & n^Last = "travel"
 // n = single date counted once.
 // p is the next project.
+// m is the number of days between projects
 // (n^(n+1) == p^first) || (n == p) = "full"
+// this is when m is greater than 1.
+// m > 1 n^last = "travel" & p^first = "travel"
+
+
+
 //  n^last > n < 1st = "full"
+
+
+//Questions 
+// - What if two dates of different rate
+//    overlap which one rate is taken? Probably the higher one.
+// - "If there is a gap between projects, then the days on
+//    either side of that gap are travel days. Does that mean
+//    in addition one day including the travel days from the
+//    first to last of a project?
